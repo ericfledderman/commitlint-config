5b5c6596735bc5a7a8b45f75f7abc55f6ae1ff81
Eric Fledderman <ericfledderman@me.com>
2021-08-15 06:23:18 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends : [
+    // All available rules
+    './rules/all'
+  ]
+}
---
+++ rules/all.js
@@ -0,0 +1,58 @@
+// 0 = off
+// 1 = warn
+// 2 = error
+
+module.exports = {
+  rules : {
+    // Body
+    'body-full-stop'       : 0,
+    'body-leading-blank'   : 0,
+    'body-empty'           : 0,
+    'body-max-length'      : 0,
+    'body-max-line-length' : 0,
+    'body-min-length'      : 0,
+    'body-case'            : 0,
+
+    // Footer
+    'footer-leading-blank'   : 0,
+    'footer-empty'           : 0,
+    'footer-max-length'      : 0,
+    'footer-max-line-length' : 0,
+    'footer-min-length'      : 0,
+
+    // Header
+    'header-case'       : 0,
+    'header-full-stop'  : 0,
+    'header-max-length' : 0,
+    'header-min-length' : 0,
+
+    // References
+    'references-empty' : 0,
+
+    // Scope
+    'scope-enum'       : 0,
+    'scope-case'       : 0,
+    'scope-empty'      : 0,
+    'scope-max-length' : 0,
+    'scope-min-length' : 0,
+
+    // Subject
+    'subject-case'             : 0,
+    'subject-empty'            : 0,
+    'subject-full-stop'        : 0,
+    'subject-max-length'       : 0,
+    'subject-min-length'       : 0,
+    'subject-exclamation-mark' : 0,
+
+    // Type
+    'type-enum'       : 0,
+    'type-case'       : 0,
+    'type-empty'      : 0,
+    'type-max-length' : 0,
+    'type-min-length' : 0,
+
+    // Misc
+    'signed-off-by'  : 0,
+    'trailer-exists' : 0,
+  }
+}
